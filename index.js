d80036ff7612674a414e78e5d0014fa29d1e8c31
Ali Talip COŞKUN <ali_talip_18@hotmail.com>
2022-10-02 18:02:50 +0300

    Randomizing Maze
    Trying to add maze algorithm

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {Engine, Render, Runner, World, Bodies} = Matter;// It is for using Matter.js library
 //for reading purposes
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -39,5 +40,45 @@ const walls = [
 
 
 World.add(world, walls);
+
+//Maze Generation
+
+const grid = Array(cells).fill(null).map(() => {
+    return Array(cells).fill(false)
+});
+
+const verticals = Array(cells).fill(null).map(() => Array(cells-1).fill(false));
+const horizontals = Array(cells-1).fill(null).map(() => Array(cells)).fill(false));
+
+const startRow = Math.floor(Math.random()*cells);
+const startColumn = Math.floor(Math.random()*cells);
+
+const stepThroughCell = (row,column) => {
+    //If I have visited the cell at [row, column] then return
+    if(grid[row][column] === true){
+        return;
+    }
+    //Mark this sell as being visited.
+    grid[row][column] = true;
+    //Assemble randomly-ordered list of neighbors
+    const neighbors = [
+        [row-1, column],
+        [row, column-1],
+        [row+1, column],
+        [row, column+1]
+    ]
+    //FOR EACH NEIGHBOR
+
+    //See if that neighbor is out of bounds
+
+    //If we have visited that neighbor, continue to next neighbor
+
+    //Remove a wall from either horizontals and verticals
+
+    //Visit next cell
+};
+
+
+stepThroughCell(startRow,startColumn);
 
 
